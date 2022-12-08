4edf5df423214becb05b9a1c3ed7d0b92de220f9
jamhea23 <83547828+jamhea23@users.noreply.github.com>
2022-12-09 00:30:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function copyPw1() {
     snackbarEl.className = "show"
     setTimeout(function(){ snackbarEl.className = snackbarEl.className.replace("show", ""); }, 1500);
     
-    console.log("Copied 👍")
+    console.log("Copied 👍" + copiedTxt1)
 }
 
 function copyPw2() {
@@ -58,7 +58,7 @@ function copyPw2() {
     snackbarEl.className = "show"
     setTimeout(function(){ snackbarEl.className = snackbarEl.className.replace("show", ""); }, 1500);
 
-    console.log("Copied 👍")
+    console.log("Copied 👍" + copiedTxt2)
     
 }
 
